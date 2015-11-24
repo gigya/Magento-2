85c57091fb174270972c5603672c66d314f88662
luciodiri <guy.avivi@gigya-inc.com>
2015-11-24 12:06:10 +0200

    Include gigya socialize script

---
+++ Block/GigyaScript.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Manage adding Gigya script with API key and global variables
+ * Defined in view/frontend/layout/default.xml
+ */
+namespace Gigya\GigyaM2\Block;
+use Magento\Framework\View\Element\Template;
+
+class GigyaScript extends Template
+{
+    protected function _prepareLayout()
+    {
+
+    }
+
+}
