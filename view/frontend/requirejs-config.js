1d96b5a57ebde344f0bad980e95148b6d43940d1
luciodiri <guy.avivi@gigya-inc.com>
2015-12-21 11:48:35 +0200

    Registration flow minimal working

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    map: {
+        '*': {
+            checkoutBalance:    'Magento_Customer/js/checkout-balance',
+            address:            'Magento_Customer/address',
+            setPassword:        'Magento_Customer/set-password'
+        }
+    }
+};
