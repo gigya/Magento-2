910966f43639b5cb3a3e9eaeec16819f286aae92
luciodiri <guy.avivi@gigya-inc.com>
2016-04-21 13:38:14 +0300

    add missing file

---
+++ Logger/Handler.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Gigya\GigyaM2\Logger;
+
+use Monolog\Logger;
+
+class Handler extends \Magento\Framework\Logger\Handler\Base
+{
+    /**
+     * Logging level
+     * @var int
+     */
+    protected $loggerType = Logger::INFO;
+
+    /**
+     * File name
+     * @var string
+     */
+    protected $fileName = '/var/log/gigya.log';
+}
