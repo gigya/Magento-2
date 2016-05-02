07aca75f00f02cb30d65fd0077a424311a2a8cae
luciodiri <guy.avivi@gigya-inc.com>
2016-05-02 15:58:58 +0300

    remove default config

--- sdk/gigya_config.php
+++ sdk/gigya_config.php
@@ -2,9 +2,9 @@
 /*
  * local API settings constants
  */
-define("API_KEY","3_nJILE6pHcAcV_PzORmiO_Y1PYCxRz1ViQySoc_PP78KzgCSrDyvcWrnNeXeO3g9A");
+define("API_KEY","");
 define("API_DOMAIN","us1.gigya.com");
-define("APP_KEY","ANxj7nGHee98");
+define("APP_KEY","");
 
 // set to "file" if app secret is saved in file, or "env", if app secret is set as server env var:
 define("KEY_SAVE_TYPE", "file"); // "file" / "env" 
