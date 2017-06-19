da5242cf86e6cb49ffc624fd71b9c68b6811c971
Vincent Lemaire <vlemaire@x2i.fr>
2017-06-19 11:34:35 +0200

    [#GIG-87] Default gender field mapping implementation

---
+++ Observer/DefaultGigyaSyncFieldMapping.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © 2016 X2i.
+ */
+
+namespace Gigya\GigyaIM\Observer;
+
+
+use Gigya\CmsStarterKit\user\GigyaProfile;
+use Magento\Customer\Model\Data\Customer;
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * DefaultCMSSyncFieldMapping
+ *
+ * Default cms2g field mapping implementation. For now only attribute gender.
+ *
+ * To be effective one have to declare this observer on event 'gigya_pre_field_mapping'.
+ *
+ * @author      vlemaire <info@x2i.fr>
+ */
+class DefaultGigyaSyncFieldMapping implements ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        /** @var Customer $customer */
+        $magentoCustomer = $observer->getData('customer');
+        // 'Translate' the gender code from Magento to Gigya value
+        switch($magentoCustomer->getGender()) {
+            case '1':
+                $magentoCustomer->setGender('m');
+                break;
+
+            case '2':
+                $magentoCustomer->setGender('f');
+                break;
+
+            default:
+                $magentoCustomer->setGender('u');
+        }
+    }
+}
