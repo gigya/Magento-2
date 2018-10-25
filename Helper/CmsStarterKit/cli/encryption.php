603759999018e7e49887448c8dd3bcf5685af14d
Ynhockey <ynhockey@gmail.com>
2018-10-25 16:16:15 +0300

    Fixing CMS starter kit path in encryption script

--- Helper/CmsStarterKit/cli/encryption.php
+++ Helper/CmsStarterKit/cli/encryption.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "GigyaApiHelper.php";
+include_once __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "GigyaApiHelper.php";
 
 set_error_handler('exceptions_error_handler');
 
