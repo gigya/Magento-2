3692026ad4887863fb548c984d8a88b3dfb66c82
Ébano Penha Andrello Lopes <ebano.lopes@osf-commerce.com>
2019-04-15 00:45:54 -0300

    GMSP-12: Fix javascript error on login

---
+++ view/frontend/web/js/customer-data.js
@@ -0,0 +1,19 @@
+define(function () {
+    'use strict';
+
+    return function (Component) {
+        Component.reloadOriginal = Component.reload;
+        Component.reload = function (sectionNames, updateSectionId) {
+            if (typeof gigya_enabled !== "undefined" && gigya_enabled && gigya_login_in_progress == true) {
+                console.log('ATTENTION: login fix triggered');
+                return;
+            }
+
+            console.log('login fix bypassed');
+
+            return this.reloadOriginal(sectionNames, updateSectionId);
+        }
+
+        return Component;
+    }
+});
