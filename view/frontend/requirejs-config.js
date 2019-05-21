91df082c931b03c314c4067b9f4135faec74493f
Yan Nasonov <ynhockey@gmail.com>
2019-05-21 11:38:35 +0300

    Adding waiting time for container element for showScreenSet

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,8 +1,8 @@
 var config = {
     map: {
         '*': {
-            gigya_script : 'Gigya_GigyaIM/js/gigya_script',
-            'Magento_Customer/js/model/authentication-popup' : 'Gigya_GigyaIM/js/model/authentication-popup'
+            'Magento_Customer/js/model/authentication-popup' : 'Gigya_GigyaIM/js/model/authentication-popup',
+            gigya_script : 'Gigya_GigyaIM/js/gigya_script'
         }
     },
     shim: {
